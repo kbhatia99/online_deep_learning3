83d0ca1e3af58a110b677ee701d875ca3e581ab6
kbhatia99 <khushbirb7@gmail.com>
2025-03-15 03:47:55 +0000

    resolved 2gb error

--- homework3/homework/datasets/classification_dataset.py
+++ homework3/homework/datasets/classification_dataset.py
@@ -38,9 +38,10 @@ class SuperTuxDataset(Dataset):
             xform = transforms.Compose(
                 [
                     transforms.RandomHorizontalFlip(),
-                    transforms.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.5),
-                    transforms.RandomRotation(30),
+                    transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
+                    transforms.RandomRotation(15),
                     transforms.RandomResizedCrop(64, scale=(0.8, 1.0)),
+                    transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
                     transforms.ToTensor(),
                 ]
             )
